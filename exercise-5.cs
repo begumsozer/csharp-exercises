3aa0b5beba347b2283f903d541486f57fd836219
Begum Sozer <55942612+begumsozer@users.noreply.github.com>
2020-07-12 14:49:20 +0300

    Update exercise-5.cs
    returns the mmultiplication table of an imput number and stars
    ex: 3
    *
    **
    ***
    
    3x1=3
    3x2=6
    3x3=9
    ...

--- exercise-5.cs
+++ exercise-5.cs
@@ -1,4 +1,15 @@
- private void button2_Click(object sender, EventArgs e)
+ 
+private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            listBox1.Items.Add(sayi + "sayının çarpım tablosu");
+            for  (int i = 0; i<=10; i++)
+            {
+                listBox1.Items.Add(sayi + "*" + i + "=" + sayi*i);
+            }
+            
+        }
+private void button2_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
             string yildiz = "*";
